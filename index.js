d137404f949d1599def04c88ed60d77b95bb7270
Mariano Salguero <salguero.ms@gmail.com>
2021-04-11 00:31:10 -0300

    Add generate_token function

--- index.js
+++ index.js
@@ -19,19 +19,28 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Server up");
 });
 
-app.get('/get_token', async (req, res) => {
-    if (req.query.refresh_token != undefined) {
-        res.json(await get_token(req.query.refresh_token));
+app.get('/generate_token', async (req, res) => {
+    if (req.query.code != undefined) {
+        res.json(await generate_token(req.query.code));
     } else {
         res.status(400).json({ "message": "bad request" });
     }
 });
 
+app.get('/refresh_token', async (req, res) => {
+    if (req.query.refresh_token != undefined) {
+        res.json(await refresh_token(req.query.refresh_token));
+    } else {
+        res.status(400).json({ "message": "bad request" });
+    }
+});
+
+
 /*********************************************************/
 
 const url_meli_api = "https://api.mercadolibre.com/";
 
-async function get_token(refresh_token) {
+async function refresh_token(refresh_token) {
     let url = new URL(url_meli_api + "oauth/token");
 
     let params = {
@@ -50,4 +59,26 @@ async function get_token(refresh_token) {
     let json = await repsonse.json();
 
     return { access_token: json.access_token };
+}
+
+async function generate_token(code) {
+    let url = new URL(url_meli_api + "oauth/token");
+
+    let params = {
+        "grant_type": "authorization_code",
+        "client_id": process.env.client_id,
+        "client_secret": process.env.client_secret,
+        "code": code,
+        "redirect_uri": process.env.redirect_uri
+    }
+
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+    let repsonse = await fetch(url, {
+        method: 'POST'
+    });
+
+    let json = await repsonse.json();
+
+    return { access_token: json.access_token, refresh_token: json.refresh_token };
 }
